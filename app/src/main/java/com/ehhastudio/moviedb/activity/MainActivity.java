f4b04e516a6895f8cc71f1b15c3d8424888462e7
Aufa Risyadi <aoefaa27@gmail.com>
2020-11-21 08:52:38 +0700

    Aufa

--- app/src/main/java/com/ehhastudio/moviedb/activity/MainActivity.java
+++ app/src/main/java/com/ehhastudio/moviedb/activity/MainActivity.java
@@ -51,7 +51,7 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onFailure(Call<Response> call, Throwable t) {
-
+                t.fillInStackTrace();
             }
         });
 
--- app/src/main/java/com/ehhastudio/moviedb/adapter/RecyclerMoviesAdapter.java
+++ app/src/main/java/com/ehhastudio/moviedb/adapter/RecyclerMoviesAdapter.java
@@ -1,22 +1,27 @@
 package com.ehhastudio.moviedb.adapter;
 
 import android.content.Context;
+import android.content.Intent;
+import android.os.Parcelable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.RelativeLayout;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.ehhastudio.moviedb.R;
+import com.ehhastudio.moviedb.activity.DetailActivity;
 import com.ehhastudio.moviedb.model.Result;
 
 import java.util.List;
 
 public class RecyclerMoviesAdapter extends RecyclerView.Adapter<RecyclerMoviesAdapter.MyViewHolder> {
+
     private Context mContext;
     private List<Result> resultList;
 
@@ -31,7 +36,20 @@ public class RecyclerMoviesAdapter extends RecyclerView.Adapter<RecyclerMoviesAd
         View view;
         LayoutInflater inflater = LayoutInflater.from(parent.getContext());
         view = inflater.inflate(R.layout.item_movie, parent, false);
-        RecyclerMoviesAdapter.MyViewHolder viewHolder = new RecyclerMoviesAdapter.MyViewHolder(view);
+
+        final RecyclerMoviesAdapter.MyViewHolder viewHolder = new RecyclerMoviesAdapter.MyViewHolder(view);
+        viewHolder.relativeLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(parent.getContext(), DetailActivity.class);
+                Result result = new Result();
+                result.setOriginalTitle(resultList.get(viewHolder.getAdapterPosition()).getOriginalTitle());
+                result.setOverview(resultList.get(viewHolder.getAdapterPosition()).getOverview());
+                result.setPosterPath(resultList.get(viewHolder.getAdapterPosition()).getPosterPath());
+                i.putExtra(DetailActivity.EXTRA_MOVIE, (Parcelable) result);
+                parent.getContext().startActivity(i);
+            }
+        });
         return viewHolder;
     }
 
@@ -41,24 +59,27 @@ public class RecyclerMoviesAdapter extends RecyclerView.Adapter<RecyclerMoviesAd
         holder.tvDescription.setText(resultList.get(position).getOverview());
 
         Glide.with(mContext)
-                .load("" + resultList.get(position).getPosterPath())
+                .load("https://image.tmdb.org/t/p/w185" + resultList.get(position).getPosterPath())
                 .into(holder.ivPoster);
     }
 
     @Override
     public int getItemCount() {
+
         return resultList.size();
     }
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
         ImageView ivPoster;
         TextView tvTitle,tvDescription;
+        RelativeLayout relativeLayout;
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
 
             ivPoster = itemView.findViewById(R.id.imgMovie);
             tvTitle = itemView.findViewById(R.id.tvTitle);
             tvDescription = itemView.findViewById(R.id.tvDeskripsi);
+            relativeLayout = itemView.findViewById(R.id.layoutMovie);
         }
     }
 }
